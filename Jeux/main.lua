7899c6858f38096e6916fa2ab01c7bef39d6a54b
Axel G <axel-gauthier@hotmail.fr>
2021-01-30 19:40:24 +0100

    refactor: tree refactoring to match Ugo's one

--- Jeux/main.lua
+++ Jeux/main.lua
@@ -1,50 +1,46 @@
 function love.load()
-
     require("source/startup/gameStart")
     gameStart()
+    mapCreation()
 
     pierre = physical_decoration:new(-35, 10, sprites.pierre, 50, 50)
     pierre_1 = physical_decoration:new(100, 60, sprites.pierre, 50, 50)
-
 end
 
 function love.update(dt)
-
     player:update(dt)
     world:update(dt)
-    cam:update()
-
+    cam:update(dt)
+    if player.isAlive == false then
+        death_modal = Modal:new("centered", 200, 150, {top=10, bottom=10, left=10, right=10})
+        death_modal:setImageBackground(love.graphics.newImage("Source/Assets/death_modal_background.png"))
+        death_modal:displayText("You are dead")
+    end
 end
 
 function love.draw()
     camera:attach()
-
-        -- Draw the background image
         love.graphics.setColor(1, 1, 1, 1)
-        -- set background and set the origine (here, width/2 and height/2)
         love.graphics.draw(sprites.background, -2500/2, -2500/2)
 
         pierre:draw()
         pierre_1:draw()
 
         player:draw()
-
-
         love.graphics.setLineWidth(5)
-        --world:draw()
-
-
     camera:detach()
     love.graphics.print(camera.scale, 0, 0)
-
+    if player.isAlive == false then
+        death_modal:drawModal()
+    end
 end
 
 function love.keypressed(key)
-
-    -- Resets the player's position to (0, 0)
-    if key == "r" then
-        player.collider:setPosition(0, 0)
-        player.collider:setLinearVelocity(0, 0)
+    if key == "k" then
+        player:die()
     end
 
+end
+
+function love.keyreleased(key)
 end
