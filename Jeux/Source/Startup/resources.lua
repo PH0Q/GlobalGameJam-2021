601559c8b4420536707b763194764181f663f23f
gogonouze <ugo.eblevadoux@g-mail.fr>
2021-01-31 12:20:04 +0100

    feat: add music

--- Jeux/Source/Startup/resources.lua
+++ Jeux/Source/Startup/resources.lua
@@ -5,3 +5,13 @@ sprites.player = love.graphics.newImage("Source/Assets/sprite_bitmap_place_holde
 sprites.background = love.graphics.newImage("Source/Assets/worldmap.png")
 sprites.death_modal_background = love.graphics.newImage("Source/Assets/death_modal_background.png")
 sprites.pierre = love.graphics.newImage("Source/Assets/pierre_1.png")
+
+music = {}
+music.dawn = love.audio.newSource("Source/Music/Matin.mp3", "stream")
+music.dawn:setVolume(0.8)
+music.afternoon = love.audio.newSource("Source/Music/Midi.mp3", "stream")
+music.afternoon:setVolume(0.8)
+music.sunset = love.audio.newSource("Source/Music/Soir.mp3", "stream")
+music.sunset:setVolume(0.8)
+music.night = love.audio.newSource("Source/Music/Nuit.mp3", "stream")
+music.night:setVolume(0.8)
